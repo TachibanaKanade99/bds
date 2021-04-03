3b90a69b5ca915e96060e8e739993aa177693cd8
Tuan Minh <sad361975@gmail.com>
2021-04-03 17:56:14 +0700

    add some arguments for webdriver

--- bds_crawler/batdongsancomvn/land_crawler/land_crawler/middlewares.py
+++ bds_crawler/batdongsancomvn/land_crawler/land_crawler/middlewares.py
@@ -117,8 +117,13 @@ class SeleniumDownloaderMiddleware:
     # Scrapy middleware for handling the requests using Selenium:
     def __init__(self):
         options = webdriver.ChromeOptions()
-        options.add_argument('headless')
+        options.add_argument('--headless')
         options.add_argument('window-size=1200x600')
+        options.add_argument('no-sandbox')
+        options.add_argument('no-default-browser-check')
+        options.add_argument('disable-gpu')
+        options.add_argument('disable-default-apps')
+        options.add_argument('disable-extensions')
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
         self.driver = webdriver.Chrome('D:\\Tuan_Minh\\bds\\bds_crawler\\batdongsancomvn\\chromedriver.exe', options=options)
         self.driver.minimize_window()
