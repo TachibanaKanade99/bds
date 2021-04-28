f112de486b7d7a0fdd5a1d7f755aee29b26e0434
Tuan Minh <sad361975@gmail.com>
2021-04-28 17:52:50 +0700

    add model evaluation for district 10

---
+++ price_prediction_model/districts/10/district_10.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('D:\\Tuan_Minh\\bds\\price_prediction_model')
+from models import evaluate
+
+evaluate.evaluateModel('10', 'Bán đất')
+evaluate.evaluateModel('10', 'Bán nhà riêng')
