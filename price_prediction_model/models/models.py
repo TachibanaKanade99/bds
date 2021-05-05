2a58ad98f77380af0db7e52665a044b59bf957fc
Tuan Minh <sad361975@gmail.com>
2021-05-05 18:08:44 +0700

    Update models.py

--- price_prediction_model/models/models.py
+++ price_prediction_model/models/models.py
@@ -30,37 +30,39 @@ def polynomialRegression(X_train, Y_train):
     X_train_poly = polynomialTransform(X_train, 2)
     selected_model, min_rmse = linearRegressionModel(X_train_poly, Y_train)
     selected_degree = 2
+    selected_X_train_poly = X_train_poly
 
     min_degree = 3
-    max_degree = 50
+    max_degree = 20
 
     for i in range(min_degree, max_degree+1):
         X_train_poly = polynomialTransform(X_train, i)
+
+        # poly_model, rmse = linearRegressionModel(X_train_poly, Y_train)
         poly_model, rmse = linearRegressionModel(X_train_poly, Y_train)
+        # poly_model, rmse = lassoRegressionModel(X_train_poly, Y_train)
 
         # Try to select the model with minimum rmse:
         if rmse < min_rmse:
             min_rmse = rmse
             selected_model = poly_model
             selected_degree = i
+            selected_X_train_poly = X_train_poly
 
+    # selected_model, min_rmse = lassoRegressionModel(selected_X_train_poly, Y_train)
     return selected_model, min_rmse, selected_degree
 
 # Lasso Model:
-def lassoRegressionModel(X_train, Y_train, X_test, Y_test):
-    model = linear_model.Lasso(alpha=1.0)
+def lassoRegressionModel(X_train, Y_train):
+    model = linear_model.Lasso(alpha=0.001, tol=0.5)
 
     # Training process:
     model.fit(X_train, Y_train)
 
-    # Evaluating the model
-    print("\nLasso Model:")
-    yfit = model.predict(X_test)
-    plt.scatter(X_train, Y_train, marker='o')
-    plt.plot(X_test, yfit)
-    plt.show()
+    # find Y_pred by model prediction:
+    Y_train_pred = model.predict(X_train)
 
-    # Evaluation the model:
-    score_trained = model.score(X_test, Y_test)
+    # Find RMSE of the linear model:
+    linear_rmse = np.sqrt(mean_squared_error(Y_train, Y_train_pred))
 
-    return score_trained 
+    return model, linear_rmse 
