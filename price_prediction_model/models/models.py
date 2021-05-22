22872b14864f2ade8b13931cf78f26cbc167b9f6
Tuan Minh <sad361975@gmail.com>
2021-05-22 17:36:19 +0700

    add CV for choosing model and tune alpha hyperparameter

--- price_prediction_model/models/models.py
+++ price_prediction_model/models/models.py
@@ -22,22 +22,25 @@ def polynomialTransform(dataset, degree):
 def calcRMSE(model, X, Y):
     return np.sqrt(mean_squared_error(Y, model.predict(X)))
 
-def polynomialRegression(X_train, Y_train, X_test, Y_test, X_validate, Y_validate):
+def calcCV(model, X, Y):
+    return np.mean(cross_val_score(model, X, Y, cv=5))
+
+def polynomialRegression(X, Y, X_train, Y_train, X_test, Y_test, X_validate, Y_validate):
 
     # degree = 2
     X_train_poly = polynomialTransform(X_train, 2)
-    # selected_model = linearRegressionModel(X_train_poly, Y_train)
-    selected_model = lassoRegressionModel(X_train_poly, Y_train)
+    selected_poly_model = linearRegressionModel(X_train_poly, Y_train)
+    # selected_model = lassoRegressionModel(X_train_poly, Y_train)
     # selected_model = ridgeRegressionModel(X_train_poly, Y_train)
     # selected_model = elasticNetRegressionModel(X_train_poly, Y_train)
     
     # calc rmse on validated data:
     X_validate_poly = polynomialTransform(X_validate, 2)
-    min_rmse = calcRMSE(selected_model, X_validate_poly, Y_validate)
+    min_poly_validate_rmse = calcRMSE(selected_poly_model, X_validate_poly, Y_validate)
 
     # Choose model with specific degree:
     selected_degree = 2
-    # selected_X_train_poly = X_train_poly
+    selected_X_train_poly = X_train_poly
 
     min_degree = 3
     max_degree = 100
@@ -45,33 +48,69 @@ def polynomialRegression(X_train, Y_train, X_test, Y_test, X_validate, Y_validat
     for i in range(min_degree, max_degree+1):
         X_train_poly = polynomialTransform(X_train, i)
 
-        # poly_model = linearRegressionModel(X_train_poly, Y_train)
-        poly_model = lassoRegressionModel(X_train_poly, Y_train)
+        poly_model = linearRegressionModel(X_train_poly, Y_train)
+        # poly_model = lassoRegressionModel(X_train_poly, Y_train)
         # poly_model = ridgeRegressionModel(X_train_poly, Y_train)
         # poly_model = elasticNetRegressionModel(X_train_poly, Y_train)
 
         # calc rmse on validated data:
         X_validate_poly = polynomialTransform(X_validate, i)
-        rmse = calcRMSE(poly_model, X_validate_poly, Y_validate)
+        validate_rmse = calcRMSE(poly_model, X_validate_poly, Y_validate)
 
         # Try to select the model with minimum rmse:
-        if rmse < min_rmse:
-            min_rmse = rmse
-            selected_model = poly_model
+        if validate_rmse < min_poly_validate_rmse:
+            min_poly_validate_rmse = validate_rmse
+            selected_poly_model = poly_model
             selected_X_train_poly = X_train_poly
             selected_degree = i
+
+    print("Polynomial Regression with degree = {} and rmse = {}".format(selected_degree, min_poly_validate_rmse))
+    
+    # Apply Ridge Regression:
+    alphas = [0.00001, 0.00003, 0.00005, 0.00008, 0.0001, 0.0003, 0.0005, 0.0008, 0.001, 0.005, 0.01, 0.02, 0.04, 0.06, 0.1, 1.0, 3.0, 10.0, 50.0, 100.0]
+
+    selected_alpha = alphas[0]
+    selected_regularized_model = ridgeRegressionModel(selected_X_train_poly, Y_train, selected_alpha)
+    # model = lassoRegressionModel(selected_X_train_poly, Y_train, alphas[0])
+
+    X_validate_poly = polynomialTransform(X_validate, selected_degree)
+    min_regularized_validate_rmse = calcRMSE(selected_regularized_model, X_validate_poly, Y_validate)
+
+    for alpha in alphas[1:]:
+        model = ridgeRegressionModel(selected_X_train_poly, Y_train, alpha)
+        # model = lassoRegressionModel(selected_X_train_poly, Y_train, alpha)
+
+        X_validate_poly = polynomialTransform(X_validate, selected_degree)
+        validate_rmse = calcRMSE(model, X_validate_poly, Y_validate)
+
+        if validate_rmse < min_regularized_validate_rmse:
+            min_regularized_validate_rmse = validate_rmse
+            selected_alpha = alpha
+            selected_regularized_model = model
+
+    print("Regularized Regression with alpha = {} and rmse = {}".format(selected_alpha, min_regularized_validate_rmse))
+
+    # Choose between Ridge Regression and Polynomial Regression using cross validation score:
+    poly_model_cv_score = calcCV(selected_poly_model, X, Y)
+    print("Polynomial Regression cross validation score: ", poly_model_cv_score)
+
+    regularized_model_cv_score = calcCV(selected_regularized_model, X, Y)
+    print("Regularized Regression cross validation score: ", regularized_model_cv_score)
+
+    selected_model = selected_poly_model if poly_model_cv_score > regularized_model_cv_score else selected_regularized_model
+    selected_validate_rmse = min_poly_validate_rmse if selected_model == selected_poly_model else min_regularized_validate_rmse
+
+    # calc rmse on train data:
+    train_rmse = calcRMSE(selected_model, selected_X_train_poly, Y_train)
 
     # calc rmse on test data:
     X_test_poly = polynomialTransform(X_test, selected_degree)
     test_rmse = calcRMSE(selected_model, X_test_poly, Y_test)
 
-    # using model with regularization for selected X_degree
-    # selected_model, min_rmse = ridgeRegressionModel(selected_X_train_poly, Y_train)
-
-    return selected_model, selected_degree, min_rmse, test_rmse
+    return selected_model, selected_degree, train_rmse, selected_validate_rmse, test_rmse
 
 # Lasso Model using L1 Regularization:
-def lassoRegressionModel(X_train, Y_train, alpha=0.005, normalize=True, max_iter=2000, tol=0.001):
+def lassoRegressionModel(X_train, Y_train, alpha, normalize=True, max_iter=2000, tol=0.001):
     model = linear_model.Lasso(alpha=alpha, normalize=normalize, max_iter=max_iter, tol=tol)
 
     # Training process:
@@ -80,7 +119,7 @@ def lassoRegressionModel(X_train, Y_train, alpha=0.005, normalize=True, max_iter
     return model
 
 # Ridge Regression using L2 Regularization:
-def ridgeRegressionModel(X_train, Y_train, alpha=0.005, normalize=True, max_iter=2000):
+def ridgeRegressionModel(X_train, Y_train, alpha, normalize=True, max_iter=2000):
     model = linear_model.Ridge(alpha=alpha, normalize=normalize, max_iter=max_iter)
 
     # Training process:
