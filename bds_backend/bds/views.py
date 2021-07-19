cffa98fe34659058c080a7f253c1533d01c1006f
Tuan Minh <sad361975@gmail.com>
2021-07-19 10:47:08 +0700

    Update views.py

--- bds_backend/bds/views.py
+++ bds_backend/bds/views.py
@@ -32,6 +32,7 @@ Price predition libraries
 """
 
 # import FunctionTransformer from sklearn.preprocessing:
+from sklearn import linear_model
 from sklearn.preprocessing import FunctionTransformer, PolynomialFeatures
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_squared_error
@@ -42,7 +43,7 @@ import sys
 sys.path.append('D:\\Tuan_Minh\\bds\\price_prediction_model')
 from models.prepareData import getData, preprocessData, convertData
 from models.models import localOutlierFactor, linearRegressionModel, polynomialRegression, regularizedRegression
-from models.models import calcRMSE 
+from models.models import calcRMSE, calcCV 
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
     def enforce_csrf(self, request):
@@ -484,7 +485,7 @@ class WardLst(APIView):
         query = query.exclude(ward__exact=None)
 
         if request_page == "predict":
-            query = query.annotate(count_street=Count('street')).filter(count_street__gt=40)
+            query = query.annotate(count_street=Count('street')).filter(count_street__gt=60)
         
         # update full query:
         query = query.order_by('ward').distinct()
@@ -521,7 +522,7 @@ class StreetLst(APIView):
         query = query.exclude(street__exact=None)
 
         if request_page == "predict":
-            query = query.annotate(count_street=Count('street')).filter(count_street__gt=50)
+            query = query.annotate(count_street=Count('street')).filter(count_street__gt=60)
         
         # update full query:
         query = query.order_by('street').distinct()
@@ -599,24 +600,25 @@ class TrainModel(APIView):
             area_mean = np.mean(data['area'])
             price_mean = np.mean(data['price'])
 
-            data = data[~( (data['area'] < area_mean) & (data['price'] > price_mean) )]
-            data = data[~( (data['area'] > area_mean) & (data['price'] < price_mean) )]
+            data = data[~( (data['area'] < area_mean) & (data['price'] > price_mean+1.5) )]
+            data = data[~( (data['area'] > area_mean) & (data['price'] < price_mean+1.5) )]
 
             # transform data into log1p
             data['area'] = (data['area']).transform(np.log1p)
             data['price'] = (data['price']).transform(np.log1p)
-            max_area = data['area'].max()
-            max_price = data['price'].max()
 
             # preprocessing data:
             data = preprocessData(data)
+
+            max_area = data['area'].max()
+            max_price = data['price'].max()
             
             if isEnableLOF:
                 # remove noise data:
                 data = localOutlierFactor(data, 10)
 
         # Split data & Train model:
-        if len(data) > 50:
+        if len(data) > 40:
 
             # divide data into train, validate, test data:
             train_data, test_data = train_test_split(data, test_size=0.3, random_state=4)
@@ -640,27 +642,35 @@ class TrainModel(APIView):
                 X_validate, Y_validate = convertData(validate_data)
 
                 # find model by using linear regression:
-                linear_model = linearRegressionModel(X_train, Y_train)
+                linear_regression_model = linearRegressionModel(X_train, Y_train)
 
                 # find Y by using linear model predict:
-                Y_train_pred = linear_model.predict(X_train)
+                Y_train_pred = linear_regression_model.predict(X_train)
 
                 # calculate RMSE on linear model:
-                linear_train_rmse = calcRMSE(linear_model, X_train, Y_train)
-                linear_validate_rmse = calcRMSE(linear_model, X_validate, Y_validate)
-                linear_test_rmse = calcRMSE(linear_model, X_test, Y_test)
+                linear_train_rmse = calcRMSE(linear_regression_model, X_train, Y_train)
+                linear_validate_rmse = calcRMSE(linear_regression_model, X_validate, Y_validate)
+                linear_test_rmse = calcRMSE(linear_regression_model, X_test, Y_test)
 
                 # find degree by using polynomial regression:
-                poly_model, poly_model_name, poly_degree, validate_poly_rmse = polynomialRegression(X_train, Y_train, X_validate, Y_validate)
-
-                # Optimize Polynomial Regression model using Regularization
-                regularized_model, regularized_model_name, validate_regularized_rmse = regularizedRegression(poly_degree, X_train, Y_train, X_validate, Y_validate)
+                poly_model, poly_model_name, poly_degree = polynomialRegression(X_train, Y_train, X_validate, Y_validate)
 
                 # transform X and X_test:
                 polynomial_features = PolynomialFeatures(degree=poly_degree)
                 X_train_poly = polynomial_features.fit_transform(X_train)
                 X_validate_poly = polynomial_features.fit_transform(X_validate)
                 X_test_poly = polynomial_features.fit_transform(X_test)
+
+                # Try predicting Y_poly:
+                Y_train_poly_pred = poly_model.predict(X_train_poly)
+
+                # Calculate poly_model RMSE:
+                poly_train_rmse = calcRMSE(poly_model, X_train_poly, Y_train)
+                poly_validate_rmse = calcRMSE(poly_model, X_validate_poly, Y_validate)
+                poly_test_rmse = calcRMSE(poly_model, X_test_poly, Y_test)
+
+                # Optimize Polynomial Regression model using Regularization
+                regularized_model, regularized_model_name, regularized_cv_score = regularizedRegression(poly_degree, X_train, Y_train, X_validate, Y_validate)
 
                 # Try predicting Y
                 Y_train_reg_pred = regularized_model.predict(X_train_poly)
@@ -671,28 +681,52 @@ class TrainModel(APIView):
                 reg_test_rmse = calcRMSE(regularized_model, X_test_poly, Y_test)
 
                 # Linear score:
-                linear_train_r2_score = linear_model.score(X_train, Y_train)
+                linear_train_r2_score = linear_regression_model.score(X_train, Y_train)
                 print("Linear Model score on train dataset: ", linear_train_r2_score)
-                linear_test_r2_score = linear_model.score(X_test, Y_test)
+                linear_test_r2_score = linear_regression_model.score(X_test, Y_test)
                 print("Linear Model score on test dataset: ", linear_test_r2_score)
+                print("\n")
 
                 # Poly score:
+                poly_train_r2_score = poly_model.score(X_train_poly, Y_train)
+                print("Poly Model score on train dataset: ", poly_train_r2_score)
+
+                poly_test_r2_score = poly_model.score(X_test_poly, Y_test)
+                print("Poly Model score on test dataset: ", poly_test_r2_score)
+                print("\n")
+
+                # Regularized score:
                 reg_train_r2_score = regularized_model.score(X_train_poly, Y_train)
                 print("Regularized Model score on train dataset: ", reg_train_r2_score)
                 reg_test_r2_score = regularized_model.score(X_test_poly, Y_test)
                 print("Regularized Model score on test dataset: ", reg_test_r2_score)
+                print("\n")
 
-                print("Linear validate rmse score: ", linear_validate_rmse)
-                print("Regularized validate rmse score: ", reg_validate_rmse)
+                # Calculate cv of linear and poly and reg to choose the best model:
+                linear_cv_score = calcCV(linear_model.LinearRegression(fit_intercept=True, normalize=False), X_train, Y_train, 'r2')
+                poly_cv_score = calcCV(linear_model.LinearRegression(fit_intercept=True, normalize=False), X_train_poly, Y_train, 'r2')
 
-                if linear_validate_rmse < reg_validate_rmse:
-                    best_model = linear_model
+                print("Linear CV score = ", linear_cv_score)
+                print("Poly CV score = ", poly_cv_score)
+                print("Regularized CV score: ", regularized_cv_score)
+
+                models_cv_score = {
+                    'Linear Regression': linear_cv_score,
+                    'Polynomial Regression': poly_cv_score,
+                    regularized_model_name: regularized_cv_score
+                }
+
+                max_cv_score = max(models_cv_score.values())
+                best_model_name = [key for key in models_cv_score if models_cv_score[key] == max_cv_score]
+
+                if best_model_name[0] == 'Linear Regression':
+                    best_model = linear_regression_model
                     model_name = "Linear Regression"
                     best_degree = 1
 
                     # Model coefficient and intercept:
-                    model_coef = linear_model.coef_
-                    model_intercept = linear_model.intercept_
+                    model_coef = linear_regression_model.coef_
+                    model_intercept = linear_regression_model.intercept_
 
                     # RMSE:
                     best_train_rmse = linear_train_rmse
@@ -702,7 +736,7 @@ class TrainModel(APIView):
                     best_train_r2_score = linear_train_r2_score
                     best_test_r2_score = linear_test_r2_score
 
-                    print("Best Model is Linear")
+                    print("Best Model is Linear Regression")
 
                     # Plot linear model:
                     plt.figure(figsize=(7, 4))
@@ -716,6 +750,39 @@ class TrainModel(APIView):
                     plt.ylabel('price')
                     plt.xlim(right=max_area+0.3)
                     plt.ylim(top=max_price+0.3)
+
+                elif best_model_name[0] == 'Polynomial Regression':
+                    best_model = poly_model
+                    model_name = "Polynomial Regression"
+                    best_degree = poly_degree
+
+                    # Model coefficient and intercept:
+                    model_coef = poly_model.coef_
+                    model_intercept = poly_model.intercept_
+
+                    # RMSE:
+                    best_train_rmse = poly_train_rmse
+                    best_test_rmse = poly_test_rmse
+
+                    # R2 score:
+                    best_train_r2_score = poly_train_r2_score
+                    best_test_r2_score = poly_test_r2_score
+
+                    print("Best Model is Polynomial Regression")
+
+                    # Plot linear model:
+                    plt.figure(figsize=(7, 4))
+                    plt.scatter(X_train, Y_train, marker='o', color='blue', label='train_data')
+                    plt.scatter(X_test, Y_test, marker='o', color='red', label='test_data')
+                    plt.scatter(X_validate, Y_validate, marker='o', color='green', label='validate_data')
+                    plt.plot(X_train, Y_train_poly_pred, color='black', label='train_model')
+                    plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+                    plt.tight_layout()
+                    plt.xlabel('area')
+                    plt.ylabel('price')
+                    plt.xlim(right=max_area+0.3)
+                    plt.ylim(top=max_price+0.3)
+
 
                 else:
                     best_model = regularized_model
@@ -782,7 +849,7 @@ class TrainModel(APIView):
 
         else:
             response = {
-                "message": "Data Length is empty or less than 30!",
+                "message": "Data Length is empty or less than 40 rows!",
                 "model_name": "None",
                 "degree": "None",
                 "model_coef": "None",
